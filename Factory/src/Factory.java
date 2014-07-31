cb63ec958a6be805efc32aca5f55d6c6ab3ddcb0
udarnick <peterboivin@Peters-MacBook-Pro.local>
2014-07-31 14:57:24 -0400

    delete other projects
    delete other projects

--- Factory/src/Factory.java
+++
@@ -1,8 +0,0 @@
-// Factory client code
-public class Factory {
-    public static void main(String args[]) {
-        String country = args[0];
-        Currency currency = CurrencyFactory.createCurrency(country);
-        System.out.println(currency.getSymbol());
-    }
-}
